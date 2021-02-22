512dde363b4b6619194f7f377287c6f7a4242447
hy.hong <dirtyhy@naver.com>
2021-02-22 17:43:08 +0900

    메인 최상단 섹션 작업

--- js/main.js
+++ js/main.js
@@ -2,4 +2,32 @@
 $('header .menu-btn').on('click', function() {
     $(this).toggleClass('active');
     $('header .menu-item').toggleClass('active');
+    $('header .logo').toggleClass('active');
 });
+
+// section01
+gsap.defaults({
+    ease: 'power4',
+    duration: 1
+});
+
+const mainVisualTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.top-sec',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
+        pin: true
+    }
+});
+
+mainVisualTl.from('.top-sec .wrap-img li', {
+    opacity: 0,
+    stagger: {
+        each: 0.5
+    }
+})
+.from('.top-sec .txt', {
+    opacity: 0,
+    y: 100
+})
