11067ddeebea8f18cac96540d5af6f3a191a1334
hy.hong <dirtyhy@naver.com>
2021-02-24 16:10:35 +0900

    top 버튼 작업

--- js/common.js
+++ js/common.js
@@ -4,3 +4,20 @@ $('header .menu-btn').on('click', function() {
     $('header .menu-item').toggleClass('active');
     $('header .logo').toggleClass('active');
 });
+
+// top button
+$(window).on('scroll', function() {
+    var scrollTop = $(this).scrollTop();
+
+    if (scrollTop > 200){
+        $('.top-btn').addClass('active');
+    } else {
+        $('.top-btn').removeClass('active');
+    };
+
+    $('.top-btn').on('click', function() {   
+        $('html, body').stop().animate({
+            scrollTop:0
+        });
+    });
+});
